2d71b3a439fdd651b7dcac7b6ea68b3e6d760fef
Ruskin Constant <ruskin.constant+dev@gmail.com>
2021-01-30 16:34:48 +0100

    add install_zsh function to bootstrap script

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -31,6 +31,39 @@ for file in $files; do
     ln -s $dir/$file ~/.$file
 done
 
+install_zsh () {
+# Test to see if zshell is installed.  If it is:
+if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
+    # Set the default shell to zsh if it isn't currently set to zsh
+    if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
+        chsh -s $(which zsh)
+    fi
+
+    # Install zimfw - it's already a defensively written install script so no need to check if it's already present
+    curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
+else
+    # If zsh isn't installed, get the platform of the current machine
+    platform=$(uname);
+    # If the platform is Linux, try an apt-get to install zsh and then recurse
+    if [[ $platform == 'Linux' ]]; then
+        if [[ -f /etc/redhat-release ]]; then
+            sudo yum install zsh
+            install_zsh
+        fi
+        if [[ -f /etc/debian_version ]]; then
+            sudo apt-get install zsh
+            install_zsh
+        fi
+    # If the platform is OS X, tell the user to install zsh :)
+    elif [[ $platform == 'Darwin' ]]; then
+        echo "Please install zsh, then re-run this script!"
+        exit
+    fi
+fi
+}
+
+install_zsh
+
 echo "Applying new bash profile settings, without restarting..."
 source ~/.bash_profile;
 source ~/.bashrc;
